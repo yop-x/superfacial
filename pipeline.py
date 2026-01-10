40f6c4b16ad3babc1aedb383772b757ba83ecd3d
Yop Xue <yopxue@Mac.home>
2026-01-10 01:58:50 +0100

    wrapped up pipeline

--- pipeline.py
+++ pipeline.py
@@ -6,16 +6,46 @@ from sklearn.preprocessing import normalize
 from sklearn.model_selection import GroupShuffleSplit
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
+import face_recognition
 
-def data_preprocess(yes_list, faces):
+names = []
+for filename in os.listdir('interface_face'):
+    name, ext = os.path.splitext(filename)
+    if ext.lower() in ('.jpg', '.jpeg', '.png', '.webp'):
+        names.append(name)
+
+names = sorted(names)
+
+def create_list(csv_file):
+    df = pd.read_csv('./labels/labels.csv')
+    df_yes = df[df['label']=='yes'] 
+
+    face_id = []
+    for face in df_yes['name'].values:
+        face_id.append(face.split('-')[1])
+
+    user_yes = []
+    for index, name in enumerate(names, start=1):
+        if str(index) in face_id:
+            user_yes.append(name)
+
+    return user_yes
+
+
+#faces from faces.pkl 
+def data_preprocess(yes_list, pickle_file):
+    with open(pickle_file, 'rb') as f:
+        faces = pickle.load(f)
+
     rows = []
     for person, encs in faces.items():
-        row = {'name': person}
+        for enc in encs:
+            row = {'name': person}
 
-        for i, value in enumerate(encs):
-            row[f'f{i}'] = value
+            for i, value in enumerate(enc):
+                row[f'f{i}'] = value
 
-        rows.append(row)
+            rows.append(row)
 
     df = pd.DataFrame(rows)
 
@@ -40,3 +70,38 @@ def data_preprocess(yes_list, faces):
     return X_train, X_test, y_train, y_test 
 
 
+
+def run_model(csv_file, pickle_file, test_file):
+    yes_list = create_list(csv_file)
+
+    X_train, X_test, y_train, y_test = data_preprocess(yes_list, pickle_file)
+
+    clf = LogisticRegression(
+        max_iter=2000,
+        class_weight="balanced",  # helpful if yop_yes is smaller
+        solver="lbfgs"
+    )
+
+    clf.fit(X_train, y_train)
+
+    pred = clf.predict(X_test)
+    proba = clf.predict_proba(X_test)[:, 1]
+
+    print("Accuracy:", accuracy_score(y_test, pred))
+    print("ROC AUC:", roc_auc_score(y_test, proba))
+    print(classification_report(y_test, pred, digits=4))
+
+
+    image = face_recognition.load_image_file(test_file)
+    test = face_recognition.face_encodings(image)
+
+    X_new = np.vstack(test)
+    norms = np.linalg.norm(X_new, axis=1, keepdims=True)
+    X_new = X_new / (norms + 1e-12)
+
+    proba = clf.predict_proba(X_new)[:,1]   
+    test_name = test_file.split('/')[-1].split('.')[0]
+    return f"the probability of you falling in love with {test_name} is {proba[0]}."  
+
+
+print(run_model("./labels/labels.csv", "./faces.pkl","./testing/sarp2.png"))
